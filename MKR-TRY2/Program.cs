f063ba0cf89f4b3b5fb8e6820170a94ed5488651
Олександр Гуйван <oleksandrguivan365@gmail.com>
2021-12-21 19:53:00 +0200

    <>

---
+++ MKR-TRY2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MKR_TRY2
+{
+    interface IShape
+    {
+        string sizeFigure { get; }
+        string Type();
+        string Area();
+    }
+
+    class Square : IShape
+    {
+        protected double r { get; set; }
+        public Square(double r)
+        {
+            this.r = r;
+        }
+        public string sizeFigure
+        {
+            get
+            {
+                return $"Cторона: {r}";
+            }
+        }
+        public string Area()
+        {
+            double s = r * r;
+            return $"Площа = {s.ToString("F")}";
+        }
+        public string Type()
+        {
+            return "Фiгура: квадрат";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square[] rectangle = { new Square(4.25), new Square(7), new Square(8.75) };
+            foreach (Square str in rectangle)
+            {
+                Console.WriteLine($"{str.Type()}\n{str.sizeFigure}\n{str.Area()}\n");
+            }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle("Коло", 4 , 8, 25);
+            Console.WriteLine("Фiгура    Радiус    Дiаметр   Довжина");
+            Console.WriteLine(circle1.Circle2());
+        }
+    }
+    class Circle
+    {
+        public int radius { get; set; }
+        public int diameter { get; set; }
+        public int line { get; set; }
+        public string name { get; set; }
+
+        public Circle(Circle previousCircle)
+        {
+            name = previousCircle.name;
+            radius = previousCircle.radius;
+            diameter = previousCircle.diameter;
+            line = previousCircle.line;
+        }
+        public Circle(string Name, int Radius, int Diametr, int Line)
+        {
+            name = Name;
+            radius = Radius;
+            diameter = Diametr;
+            line = Line;
+        }
+        public string Circle2()
+        {
+            return name + "         " + radius.ToString() + "      " + diameter.ToString() + "           " + line.ToString();
+        }
+    }
+}
